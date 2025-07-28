8159c8618af40717b2852657906ea3c734a45604
Ng Yi Zhe <ngyizhe@gmail.com>
2025-07-28 16:55:04 +0800

    .

--- app.js
+++ app.js
@@ -31,23 +31,6 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Middleware functions
-function checkAuthenticated(req, res, next) {
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect('/login');
-  }
-}
-
-function checkAdmin(req, res, next) {
-  if (req.session.user && req.session.user.role === 'admin') {
-    next();
-  } else {
-    res.status(403).send('Access denied. Admins only.');
-  }
-}
-s
 
 // Routes
 app.get('/', (req, res) => {
